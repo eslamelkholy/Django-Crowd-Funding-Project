55a3be562c36996ef3968b3c57dc157bcdb19cfc
eslamelkholy <eslamelkholy5325762@gmail.com>
2020-03-12 14:49:13 +0200

    project

--- project/models.py
+++ project/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=40)
+    
