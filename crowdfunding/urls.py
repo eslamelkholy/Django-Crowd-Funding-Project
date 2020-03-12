b72b38b45065d7d6eea5083a05696d880e8d3588
eslamelkholy <eslamelkholy5325762@gmail.com>
2020-03-12 14:48:34 +0200

    ok

--- crowdfunding/urls.py
+++ crowdfunding/urls.py
@@ -21,7 +21,7 @@ from project import views as project_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('addproject/',project_views.addproject)
+    path('addproject',project_views.addproject)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
