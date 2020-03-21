fa579524834b9e9a7e1acaaaccb6ec6c2e68c3dc
amiraITI2019 <amirareda.iti.intake40@gmail.com>
2020-03-21 12:43:03 +0200

    logout

--- authentication/views.py
+++ authentication/views.py
@@ -33,7 +33,7 @@ def registerView(self):
 
 def logout_user(request):
     logout(request)
-    return redirect("/product")
+    return redirect("/project")
 
 class Signup(View):
     def get(self, request):
