7bab7fb7a9ac44884dc21eb0e8c78f6ebfb50c5a
mahmoud magdy <mahmoudmagdy4318@gmail.com>
2020-03-22 00:03:53 +0200

    models updates

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
+# class User(models.Model):
     # user=models.OneToOneField(User,on_delete=models.CASCADE)
     # u_id = models.AutoField(primary_key=True)
     # fname = models.CharField(max_length=50)
@@ -13,8 +13,8 @@ class User(models.Model):
     # user_img = models.ImageField(default="",upload_to='images/')
     # email_confirmed = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.fname
+    # def __str__(self):
+    #     return self.fname
     # Create your models here.
 class Profile(models.Model):
     user=models.OneToOneField(User,null=False,on_delete=models.CASCADE)
