25f29963ac8be5fa92ff92a8c5fda7e777df901b
Hadeer-Elsaeed <hadeerelsaeed00@gmail.com>
2020-03-24 22:47:19 +0200

    last updates

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,24 +5,32 @@ from user.models import Profile
 
 class UserForm(forms.ModelForm):
 
-    u_data = Profile.objects.filter(pk=3)
-    for data in u_data:
-        if data:
-            first_name = forms.CharField(label="FirstName", widget=forms.TextInput(attrs={"value": data.user.first_name, "class":"form-control col-md-6"}))
-            last_name = forms.CharField(label="LastName", widget=forms.TextInput(attrs={"value": data.user.last_name, "class":"form-control col-md-6"}))
-            email = forms.CharField(disabled=True,required=False, label="Email", widget=forms.EmailInput(attrs={"value": data.user.email, "class":"form-control col-md-6"}))
-            password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"value": data.user.password, "class":"form-control col-md-6"}))
-            phone = forms.CharField(label="Phone", widget=forms.NumberInput(attrs={"value": data.phone, "class":"form-control col-md-6"}))
-            birthdate = forms.DateField(required=False, widget=forms.DateInput(attrs={"type":'date',"class":"form-control col-md-6"}))
-            fbprofile = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control col-md-6"}))
-            country = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control col-md-6"}))
-            user_img = forms.ImageField(required=False,label="Profile Picture")
+    first_name = forms.CharField(required= False,label="FirstName", widget=forms.TextInput(attrs={"class":"form-control col-md-6"}))
+    last_name = forms.CharField(required=False,label="LastName", widget=forms.TextInput(attrs={ "class":"form-control col-md-6"}))
+    email = forms.CharField(disabled=True,required=False, label="Email", widget=forms.EmailInput(attrs={"class":"form-control col-md-6"}))
+    password = forms.CharField(required=False,label="Password", widget=forms.PasswordInput(attrs={"class":"form-control col-md-6"}))
 
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+        ]
+
+
+class ProfileForm(forms.ModelForm):
+
+    phone = forms.CharField(required=False,label="Phone", widget=forms.NumberInput(attrs={"class":"form-control col-md-6"}))
+    birthdate = forms.DateField(required=False, widget=forms.DateInput(attrs={"type":'date',"class":"form-control col-md-6"}))
+    fbprofile = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control col-md-6"}))
+    country = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control col-md-6"}))
+    user_img = forms.ImageField(required=False,label="Profile Picture")
 
     class Meta:
         model = Profile
         fields = [
-
             'phone',
             'birthdate',
             'fbprofile',
@@ -30,10 +38,3 @@ class UserForm(forms.ModelForm):
             'user_img',
             ]
 
-
-"""
-    'user[first_name]',
-            'user.last_name',
-            'user.email',
-            'user.password',
-"""
--- profiles/views.py
+++ profiles/views.py
@@ -5,7 +5,7 @@ from project.models import Project, Donation
 from django.contrib.auth.models import User
 from user.models import Profile
 from django.contrib.auth.decorators import login_required
-from .forms import UserForm
+from .forms import UserForm,ProfileForm
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth import authenticate
 
@@ -46,19 +46,19 @@ def donations(request):
 def edit(request):
     print("request",request)
     u_data = Profile.objects.filter(user_id=request.session['id'])
+
     if request.method == "POST":
-        print(request.POST)
-        form = UserForm(request.POST,request.FILES)
-        if form.is_valid():
+        u_form = UserForm(request.POST,request.FILES)
+        p_form = ProfileForm(request.POST,request.FILES)
+        if u_form.is_valid() and p_form.is_valid():
             for data in u_data:
-                myid = request.session['id']
-                data.user.first_name = request.POST.get('fname',False)
-                data.user.last_name = request.POST.get('lname',False)
-                data.user.password = request.POST.get('password',False)
-                data.phone = request.POST.get('phone',False)
-                data.birthdate = request.POST.get('birthdate',False)
-                data.fbprofile = request.POST.get('fbprofile',False)
-                data.country = request.POST.get('country',False)
+                data.user.first_name = request.POST['first_name']
+                data.user.last_name = request.POST['last_name']
+                data.user.password = request.POST['password']
+                data.phone = request.POST['phone']
+                data.birthdate = request.POST['birthdate']
+                data.fbprofile = request.POST['fbprofile']
+                data.country = request.POST['country']
                 user_img = request.FILES.getlist('user_img')[0]
                 fs = FileSystemStorage()
                 filename = fs.save(user_img.name, user_img)
@@ -68,7 +68,10 @@ def edit(request):
             print("erorr")
 
     else:
-        form = UserForm()
+        obj1 = User.objects.get(pk=request.session['id'])
+        obj2 = Profile.objects.get(user_id=request.session['id'])
+        u_form = UserForm(instance=obj1)
+        p_form = ProfileForm(instance=obj2)
 
     p_data = Project.objects.filter(user_id=request.session['id'])
     d_data = Donation.objects.filter(user_id=request.session['id'])
@@ -77,7 +80,8 @@ def edit(request):
         'u_data': u_data,
         'p_data': p_data,
         'd_data': d_data,
-        'form': form,
+        'u_form': u_form,
+        'p_form':p_form,
     }
     return render(request, 'profiles/edit.html', context)
 
